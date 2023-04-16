4ae9a10e9ed7685dd4542207b497a265b28886ab
Parth S <93620470+parthns92@users.noreply.github.com>
2023-04-16 05:26:40 +0000

    Add tests, sources, update queries to dbt syntax

--- my_dbt_project/models/customers.sql
+++ my_dbt_project/models/customers.sql
@@ -1,11 +1,15 @@
+{{ config(
+    materialized = 'table'
+) }}
+
 select
 	orders.customer_id as customer_id,
 	customers.name as customer_name,
 	customers.email as customer_email,
 	min(orders.created_at) as first_order_at,
 	count(orders.created_at) as order_count
-from `analytics-engineers-club.coffee_shop.customers` as customers
-left join `analytics-engineers-club.coffee_shop.orders` as orders
+from {{ source('coffee_shop', 'customers') }} as customers
+left join {{ source('coffee_shop', 'orders') }} as orders
   on customers.id = orders.customer_id
 group by 
     customer_id,
